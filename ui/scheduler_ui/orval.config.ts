462a5dff31452d1d87bf4f65cca6c5ba86d60e78
Benjamin Becker <18665277+blbecker@users.noreply.github.com>
2025-08-31 15:27:19 -0400

    chore: setup orval

---
+++ ui/scheduler_ui/orval.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  scheduler_api: {
+    input: { target: './openapi.json' },
+
+    output: {
+      mode: 'tags-split',
+      target: 'src/scheduler_api.ts',
+      schemas: 'src/models',
+      client: 'vue-query',
+      mock: true,
+    },
+
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+});
